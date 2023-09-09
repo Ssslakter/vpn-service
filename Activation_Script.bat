a7646c45f13797fa4fa78cca2f48d448cc90fc1f
ssslakter <67190162+Ssslakter@users.noreply.github.com>
2023-09-09 16:27:23 +0700

    some script

---
+++ Activation_Script.bat
@@ -0,0 +1,20 @@
+﻿@echo off
+set serviceName=VpnIdeService
+
+REM Check if the service is running
+sc query %serviceName% | find "RUNNING" >nul
+
+REM Check the error level to determine if the service is running
+if %errorlevel%==0 (
+    REM The service is running, stop it
+    net stop %serviceName%
+    echo %serviceName% has been stopped.
+    REM Run the "ovpnconnector stop" command here
+    ovpnconnector stop
+) else (
+    REM The service is not running, start it
+    net start %serviceName%
+    echo %serviceName% has been started.
+)
+
+pause
